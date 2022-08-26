cfee63fd3b13904536686ecea4d9d9af9f930ea4
student <student@BC-minimac022s-Mac-mini.local>
2022-08-26 14:19:36 +0300

    add first lesson and practice part

---
+++ 1lesson/test.js
@@ -0,0 +1,45 @@
+function sum(x, y) {
+  return x + y;
+}
+
+console.log(sum(1, 3));
+console.log(sum(1, 5));
+
+// console.log("BLUE".toLocaleLowerCase());
+
+let language = "JavaScript";
+
+// console.log(language[0]);
+
+// console.log(language[1]);
+
+// console.log(language[2]);
+
+// console.log([language.length - 1]);
+
+let firstName = "James";
+let lastName = "Bond";
+let string = firstName + " " + lastName;
+
+console.log(string);
+
+let cool = "James";
+cool += "Bond";
+
+let text = `This is a multilinestring
+ that just works!`;
+console.log(text);
+
+let lowLanguage = "JavaScript";
+console.log(`I am learning now ${lowLanguage}`);
+
+let number = 45;
+console.log(number);
+console.log(number.toString());
+
+let str = "42";
+let str1 = "41";
+console.log(Number.parseInt(str, 10));
+console.log(+str1);
+
+// console.log(Number(typeof str));
