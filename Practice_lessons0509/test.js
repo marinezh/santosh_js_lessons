54b4a9646001e9a80b9abc0ea5f7474e751e97cc
student <student@BC-minimac022s-Mac-mini.local>
2022-09-05 14:59:15 +0300

    added practice lessons 0509

---
+++ Practice_lessons0509/test.js
@@ -0,0 +1,15 @@
+const isLegal = (age) => {
+  return age >= 18;
+};
+console.log(isLegal(16));
+
+const isLegal1 = (age) => age >= 18;
+
+console.log(isLegal1(16));
+
+const name = "SSam Bluee";
+// name.trim();
+console.log(name.trim());
+
+const message = "You are welcome";
+console.log();
