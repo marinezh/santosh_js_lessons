a2af4a9f29437f6188b56f204fba62e5630bad17
Zhivotova Marina <s2200724@BC-C02J64AEQ6LR-MacBook-Air.local>
2022-09-19 21:25:11 +0300

    dom misc added to 1209

--- Practice_lessons1209/DOM_misc/dom1/dom1.js
+++ Practice_lessons1209/DOM_misc/dom1/dom1.js
@@ -7,12 +7,18 @@ Also, the order of the items should be the same as the one in the array.
 So, the first item should show up first (at the top).
 
 
-/**
- * @param {string[]} items
- */
-const renderShoppingList = items => {
+// /**
+//  * @param {string[]} items
+//  */
+// const renderShoppingList = items => {
+// let itemsList = document.querySelector('#shopping-list').const renderShoppingList = (item) => {
+//     for (const item of items) {
+//         itemsList.insertAdjacentHTML('beforeend', `<li>§{item}</li>`);
+//     }
+// }
+// }
+// // Sample usage - do not modify
+// const sampleList = ["Orange", "Banana", "Coffee", "Paper"];
+// console.log(renderShoppingList(sampleList));
 
-}
-// Sample usage - do not modify
-const sampleList = ["Orange", "Banana", "Coffee", "Paper"];
-console.log(renderShoppingList(sampleList));
+// not done
