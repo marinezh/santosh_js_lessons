1f8bda830fc89d343863428c0b6507ba7e362620
Marina Zhivotova <mazhivotova@gmail.com>
2022-10-04 01:16:50 +0300

    add minore changes in js file for todo task

--- todoList/script.js
+++ todoList/script.js
@@ -1,70 +1,62 @@
-const todo = document.querySelector('.input__todo')
-const addBtn = document.querySelector('.btn__add')
-const list = document.querySelector('ul')
-// const taskInput = () => {
-//     console.log(todo.value);
-// }
-
-// todo.addEventListener('change', taskInput)
-let todoList = []
-
+const todo = document.querySelector(".input__todo");
+const addBtn = document.querySelector(".btn__add");
+const list = document.querySelector("ul");
 
 const addNewTask = () => {
-    console.log(todo.value);
+  console.log(todo.value);
 
-    // creation li elements with new tasks
-    let ul = document.querySelector(".todo")
-    let newTodo = document.createElement("li")
-    newTodo.className = "new__todo"
+  // creation li elements with new tasks
+  let ul = document.querySelector(".todo");
+  let newTodo = document.createElement("li");
+  newTodo.className = "new__todo";
 
-    newTodo.innerHTML = `<input type='checkbox' class='checkbox'><span>${todo.value}</span><button class='delete'>❌</button>`
-    ul.prepend(newTodo)
+  newTodo.innerHTML = `<input type='checkbox' class='checkbox'><span>${todo.value}</span><button class='delete'>❌</button>`;
+  ul.prepend(newTodo);
+    console.log(newTodo);
+    console.log('length', newTodo.innerHTML.length);
+  // does/t work !!!!!
+  if (newTodo.innerHTML.length > 0) {
+    document.querySelector(".two__buttons").style.display = "flex";
+  }
 
-    // does/t work !!!!!
-    if (newTodo.length > 0) {
-        document.querySelector(".two__buttons").style.display = "flex";
+  // delete done tasks
+  let deleteButton = document.querySelector(".delete");
+  let checkedItem = document.querySelector(".checkbox");
+  let doneItem = document.querySelector("span");
+
+  // if task checked add new style
+  const cheacked = () => {
+    doneItem.classList.toggle("mystyle");
+  };
+  checkedItem.addEventListener("click", cheacked);
+
+  // delete task if it is done
+  const deleteFunc = () => {
+    if (doneItem.classList.contains("mystyle")) {
+      newTodo.remove();
     }
+  };
+  deleteButton.addEventListener("click", deleteFunc);
 
-    // delete done tasks
-    let deleteButton = document.querySelector(".delete")
-    let checkedItem = document.querySelector('.checkbox')
-    let doneItem = document.querySelector("span")
+  const deleteDone = document.querySelector(".delete__done");
+  const deleteAllBtn = document.querySelector(".delete__all");
 
-    // if task checked add new style
-    const cheacked = () => {
-        doneItem.classList.toggle('mystyle')
-    }
-    checkedItem.addEventListener("click", cheacked)
+  const deletionAllfunc = () => {
+    newTodo.remove();
+      document.querySelector(".two__buttons").style.display = "none";
+      console.log('length', newTodo.innerHTML.length);
+      console.log('new', newTodo);
+  };
 
-    // delete task if it is done
-    const deleteFunc = () => {
-        if (doneItem.classList.contains('mystyle')) {
-            newTodo.remove();
-        }
-    }
-    deleteButton.addEventListener('click', deleteFunc)
+  deleteDone.addEventListener("click", deleteFunc);
+  deleteAllBtn.addEventListener("click", deletionAllfunc);
+};
+addBtn.addEventListener("click", addNewTask);
 
-    const deleteDone = document.querySelector(".delete__done");
-    const deleteAllBtn = document.querySelector(".delete__all");
-
-    const deletionAllfunc = () => {
-        newTodo.remove();
-        document.querySelector(".two__buttons").style.display = "none";
-    }
-
-
-
-    deleteDone.addEventListener('click', deleteFunc)
-    deleteAllBtn.addEventListener('click', deletionAllfunc)
-
-}
-addBtn.addEventListener('click', addNewTask)
-
-// delete button creation
-todoWrap = document.querySelector(".todo__wrap")
+// delete_buttons creation
+todoWrap = document.querySelector(".todo__wrap");
 console.log(todoWrap);
-let twoButtons = document.createElement("div")
+let twoButtons = document.createElement("div");
 twoButtons.className = "two__buttons";
-twoButtons.innerHTML = `<button class = "delete__done"> Delete Done </button> <button class = "delete__all"> Delete All </button>`
+twoButtons.innerHTML = `<button class = "delete__done"> Delete Done </button> <button class = "delete__all"> Delete All </button>`;
 todoWrap.append(twoButtons);
-
