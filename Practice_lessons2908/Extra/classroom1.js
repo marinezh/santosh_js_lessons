455d0a0d75980e966d3a549ee6340e4e886ba8dc
Marina Zhivotova <mazhivotova@gmail.com>
2022-10-18 23:05:59 +0300

    add minor changes to extra task

--- Practice_lessons2908/Extra/classroom1.js
+++ Practice_lessons2908/Extra/classroom1.js
@@ -1,28 +1,63 @@
 'use strict';
 
-/**
 
-For each of the exercises below only 11 simple excercises, assume you are starting with the following people array.
+
+// For each of the exercises below only 11 simple excercises, assume you are starting with the following people array.
 
 let people = ["Sergei", "Mariia", "Boris", "Abel"];
 
-1. Using a loop, iterate through this array and console.log all of the people (also rewrite it using arrow function).
-2. Write the command to remove "Sergei" from the array.
-3. Write the command to remove "Abel" from the array.
-4. Write the command to add "Mahalete" to the front of the array.
-5. Write the command to add your name to the end of the array.
-6. Write the command to make a copy of the array using slice. The copy should NOT include "Sergei" or "Mariia".
-7. Write the command that gives the indexOf where "Mariia" is located.
-8. Write the command that gives the indexOf where "Foo" is located (this should return -1).
-9. Redefine the people variable with the value you started with. Using the splice command, remove "Boris" from the array and add "Rakhi" and "Yulia". Your array should look like this when you are done ["Sergei", "Mariia", "Rakhi", "Yulia", "Abel"].
-10. Create a new variable called withBob and set it equal to the people array concatenated with the string of "Bob".
+// 1. Using a loop, iterate through this array and console.log all of the people (also rewrite it using arrow function).
 
-/**
- */
+people.forEach(person => console.log(person))
+
+// 2. Write the command to remove "Sergei" from the array.
+
+people.shift([0])
+console.log(people);
+
+// 3. Write the command to remove "Abel" from the array.
+
+people.pop([0])
+console.log(people);
+
+// 4. Write the command to add "Mahalete" to the front of the array.
+
+people.unshift('Mahalete')
+console.log(people);
+
+// 5. Write the command to add your name to the end of the array.
+
+people.push('Marina')
+console.log(people);
+
+// 6. Write the command to make a copy of the array using slice. The copy should NOT include "Sergei" or "Mariia".
+
+let clone = people.slice(1)
+console.log(clone);
+console.log(people);
+
+
+// 7. Write the command that gives the indexOf where "Mariia" is located.
+
+console.log(people.indexOf('Mariia'));
+
+// 8. Write the command that gives the indexOf where "Foo" is located (this should return -1).
+
+console.log(people.indexOf('Foo'));
+
+// 9. Redefine the people variable with the value you started with. Using the splice command, remove "Boris" from the array and add "Rakhi" and "Yulia". Your array should look like this when you are done ["Sergei", "Mariia", "Rakhi", "Yulia", "Abel"].
+
+// console.log('people', people);
+// let removed = people.splice(2, 0, 'Rakhi')
+// console.log('jjj', removed);
+
+// 10. Create a new variable called withBob and set it equal to the people array concatenated with the string of "Bob".
+
+let withBob = people.concat('Bob')
+console.log(withBob);
 
  /**
   * @param {people[]} strings
   */
 
-  const people = ["Sergei", "Mariia", "Boris", "Abel"];
   
--- Practice_lessons2908/Extra/classroom2.js
+++ Practice_lessons2908/Extra/classroom2.js
@@ -1,12 +1,22 @@
-'use strict';
 
 /**
 Task 1;
 let myAlphabet = ['A', 'B', 'C', 'D','E','F', 'G'];
 What is the length of the array?
-Write a function called myAlphabetLength which console.logs the length of the array
+Write a function called c which console.logs the length of the array
 Within the function also use an if-conditional statement that checks if the number of items within the array are less than 5
 **/
+let myAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+const myAlphabetLength = (myAlphabet) => {
+   console.log(myAlphabet.length);
+   if (myAlphabet.length < 5) {
+      return true 
+   } else {
+      return false
+   }
+}
+console.log(myAlphabetLength(myAlphabet))
+
 
 /**
 Task 2;
@@ -14,12 +24,20 @@ Declare a arrow function checkFunc that takes a string and a boolean as paramete
 Call the function using 2 arguments
 **/
 
-/**
-Task 3;
-Declare and initialize an array called Planets with 5 string values
-console.log each item in the array
-Also console.log the index in each iteration
-**/
+const checkFunc = (string, boolean) => {
+console.log('hei', true);
+}
+checkFunc()
+
+// Task 3;
+// Declare and initialize an array called Planets with 5 string values
+// console.log each item in the array
+// Also console.log the index in each iteration
+// 
+
+const planets = ['mars', 'venera', 'earth', 'pluton', 'sun']
+planets.forEach(planet => console.log(planet, planets.indexOf(planet)))
+
 
 /**
 Task 4:
@@ -27,13 +45,17 @@ Declare and initialize an array called wowDatatypes
 The array must have 5 different data types (NOT objects)
 Iterate over the array and console.log each item in the array + it’s index and data type in the array
 **/
+ const wowDatatypes = [5, 'string', true, undefined, NaN]
+wowDatatypes.forEach((dataType ) => console.log(wowDatatypes.indexOf(dataType), dataType, typeof(dataType)))
 
 /**
 Task 5:
 console.log each item in this array WITHOUT using a for loop
    let myArr = [ 1, 2, 'One', true];
 **/
+let myArr = [ 1, 2, 'One', true];
 
+myArr.forEach(item => console.log(item))
 /**
 Task 6:
 let student1Courses = ['Math', 'English', 'JSProgramming'];
@@ -41,9 +63,22 @@ let student2Courses = ['Geography', 'Spanish', 'JSProgramming'];
 Loop over the 2 arrays and if there are any common courses, if so console.log them
 **/
 
-/*
+let student1Courses = ['Math', 'English', 'JSProgramming'];
+let student2Courses = ['Geography', 'Spanish', 'JSProgramming'];
 
-Task 7:
-let furniture = ['Table', 'Chairs','Couch'];
-For each item in this array console.log the letters in each item
-**/
+student1Courses.forEach((course1) => {
+   if (student2Courses.includes(course1)){
+      console.log(course1)}
+
+})
+
+// Task 7:
+// let furniture = ['Table', 'Chairs','Couch'];
+// For each item in this array console.log the letters in each item
+
+let furniture = ["Table", "Chairs", "Couch"];
+furniture.forEach(item => {
+   for (let i = 0; i < item.length; i++){
+      console.log(item[i]);
+   }
+})
